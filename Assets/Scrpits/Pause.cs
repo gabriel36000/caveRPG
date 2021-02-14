c1eeac82e5b9dc9a1f24f43159c172e9649f2426
MrGabriel3600 <mgcolors3600@yahoo.com>
2021-02-13 19:37:49 -0500

    0.1.5
    Added pause panel UI
    Added cave pixel tiles
    Added mountain pixel tiles

---
+++ Assets/Scrpits/Pause.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Pause : MonoBehaviour
+{
+    private bool isPaused;
+    public GameObject pausePanel;
+    public string mainMenu;
+
+    // Start is called before the first frame update
+    public void Start()
+    {
+        isPaused = false;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+        if (Input.GetButtonDown("pause")) {
+            isPaused = !isPaused;
+            
+            if (isPaused) {
+                pausePanel.SetActive(true);
+                Time.timeScale = 0f;
+            }
+            else {
+                pausePanel.SetActive(false);
+                    Time.timeScale = 1f;
+                
+            }
+        }
+    }
+    public void Resume() {
+        isPaused = !isPaused;
+
+    }
+    public void QuitToMain() {
+        SceneManager.LoadScene(mainMenu);
+    }
+}
